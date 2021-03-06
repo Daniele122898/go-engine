17065e6bff3a29f2295651c86f71c721fc133ab3
Serenity <d.palbanese@gmail.com>
2021-03-06 21:53:47 +0100

    Added simple vertex shader support

--- cmd/engine/main.go
+++ cmd/engine/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/go-gl/glfw/v3.3/glfw"
 	"log"
 	"runtime"
+	"unsafe"
 )
 
 // Called first
@@ -13,6 +14,10 @@ func init() {
 	// See documentation for functions that are only allowed to be called from the main thread.
 	runtime.LockOSThread()
 }
+
+var (
+	vertexShaderSource = "#version 460 core\n\nlayout (location = 0) in vec3 aPos;\n\nvoid main() {\n    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n}\000"
+)
 
 func main() {
 	log.Println("Starting voxel engine")
@@ -45,6 +50,36 @@ func main() {
 	gl.Viewport(0,0,1280,720)
 	// Register callback in case user changes the window size so gl can update the viewport.
 	window.SetFramebufferSizeCallback(FramebufferSizeCallback)
+
+	// Playing around with some triangles
+	// Vertices for 1 triangle starting from bottom left going counter clock wise
+	vertices := [9]float32 {
+		-0.5, -0.5, 0.0,
+		 0.5, -0.5, 0.0,
+		 0.0,  0.5, 0.0,
+	}
+	// Tell opengl to create 1 buffer and pass us the ID
+	var VBO uint32
+	gl.GenBuffers(1, &VBO)
+	// We then bind that buffer as a type ARRAY_BUFFER
+	gl.BindBuffer(gl.ARRAY_BUFFER, VBO)
+	// From now on any call on the ARRAY_BUFFER target will configure this bound buffer.
+	// Calling glBufferData will then copy the defined vertex data into the buffers memory
+	gl.BufferData(gl.ARRAY_BUFFER, len(vertices) << 2, unsafe.Pointer(&vertices), gl.STATIC_DRAW)
+
+	// Dynamically compile the shaders
+	vertexShader := gl.CreateShader(gl.VERTEX_SHADER)
+	transformedSource := gl.Str(vertexShaderSource)
+	gl.ShaderSource(vertexShader, 1, &transformedSource, nil)
+	gl.CompileShader(vertexShader)
+	// Checking compile status
+	var success int32
+	infoLog := [512]uint8{}
+	gl.GetShaderiv(vertexShader, gl.COMPILE_STATUS, &success)
+	if success == 0 {
+		gl.GetShaderInfoLog(vertexShader, 512, nil, &infoLog[0])
+		log.Fatalf("vertex shader compilation failed %s", gl.GoStr(&infoLog[0]))
+	}
 
 	for !window.ShouldClose() {
 		// input
